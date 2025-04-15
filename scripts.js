3c32e40df74a0477dde2bb959f0fd8aa64619701
Galvin Voltag <galvinvoltag@gmail.com>
2025-04-15 17:23:07 +0300

    first changes eh

--- scripts.js
+++ scripts.js
@@ -1,75 +1,19 @@
-// function onLoadFunctions() {
-//   //some funcitons here...
-// }
-
-window.addEventListener('scroll', () => {
-  document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
-
 
 function start() {
-  document.getElementById("tab1").style.display = "block";
-  document.getElementById("btn1").style.backgroundColor = "#1f1f1f";
-  document.getElementById("btn1").style.padding = "7px 10px";
 }
 window.onload = start;
 
 function openTab(tabName, btnid) {
-  // Hide all tab content
   var i, tabcontent, buttons;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("tab_container");
   buttons = document.getElementsByClassName("tablink");
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.backgroundColor="transparent";
-    tabcontent[i].style.display="none";
+    tabcontent[i].classList.add("hidden")
   }
   for (i = 0; i < buttons.length; i++) {
-    buttons[i].style.backgroundColor = "#444";
-    buttons[i].style.padding = "5px 10px";
+    buttons[i].classList.remove("tablink_active")
   }
-  document.getElementById(btnid).style.backgroundColor = "#1f1f1f";
-  document.getElementById(btnid).style.padding = "7px 10px";
-  // this.classList.add("tablink");
-  // Show the selected tab content
-  document.getElementById(tabName).style.display="block";
-  document.getElementById(tabName).scrollIntoView();
+  document.getElementById(btnid).classList.add("tablink_active")
+  document.getElementById(tabName).classList.remove("hidden");
+  window.scrollTo(0, 0)
 }
-
-
-
-// // Smooth scrolling for anchor links
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//       e.preventDefault();
-  
-//       const targetSection = document.querySelector(this.getAttribute('href'));
-//       targetSection.scrollIntoView({
-//         behavior: 'smooth'
-//       });
-//     });
-//   });
-  
-//   // Smooth scrolling on arrow keys for sections
-//   const sections = document.querySelectorAll('section');
-  
-//   window.addEventListener('keydown', function (e) {
-//     let currentSectionIndex = Array.from(sections).findIndex(section => {
-//       const rect = section.getBoundingClientRect();
-//       return rect.top >= 0;
-//     });
-  
-//     if (e.key === 'ArrowUp') {
-//       e.preventDefault();
-//       currentSectionIndex = Math.max(currentSectionIndex - 1, 0);
-//       sections[currentSectionIndex].scrollIntoView({
-//         behavior: 'smooth'
-//       });
-//     } else if (e.key === 'ArrowDown') {
-//       e.preventDefault();
-//       currentSectionIndex = Math.min(currentSectionIndex + 1, sections.length - 1);
-//       sections[currentSectionIndex].scrollIntoView({
-//         behavior: 'smooth'
-//       });
-//     }
-//   });
-  
